94f23e63925534feb1cc3d0e951e70d2af536a5c
Danielandro <mtme13@hotmail.com>
2019-10-01 13:04:54 +0100

    Create react-bootstrap form
    Co-authored-by: lucianmot <lucian.i.mot@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> take the changes and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form>
+        <Form.Group controlId="basicForm">
+          <Form.Label>Who are you?</Form.Label>
+          <Form.Control type="text" placeholder="I said who you be?"></Form.Control>
+          <Form.Text className="text-muted">We will keep it a secret...shhh</Form.Text>
+        </Form.Group>
+        <Button variant="primary" type="submit">Submit To Me</Button>
+      </Form>
     </div>
   );
 }
