a90a1bd179156da99bc8dd9878d8284d45b735da
Danielandro <mtme13@hotmail.com>
2019-10-01 13:36:28 +0100

    Move all components into App.js
    Co-authored-by: lucianmot <lucian.i.mot@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import BasicForm from './Components/BasicForm';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+// import BasicForm from './Components/BasicForm';
 
 function App() {
   return (
     <div className="App">
       <BasicForm />
+    </div>
+  );
+}
+
+function BasicForm(props) {
+  return (
+    <div className="BasicForm">
+      <Form>
+        <Form.Group controlId="basicForm">
+          <Form.Label>Who are you?</Form.Label>
+          <Form.Control type="text" placeholder="I said who you be?"></Form.Control>
+          <Form.Text className="text-muted">We will keep it a secret...shhh</Form.Text>
+        </Form.Group>
+        <Button variant="primary" type="submit">Submit To Me</Button>
+      </Form>
     </div>
   );
 }
